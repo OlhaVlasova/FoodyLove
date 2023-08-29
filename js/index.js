0c8fb7fc6a51d140fac6ae544922e4d0d426b86d
Olga Vlasova <olga.vlasova@getta.io>
2023-08-29 19:50:18 +0200

    add adaptive

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $('.delightful-slider').slick({
+    $(".delightful-slider").slick({
         lazyLoad: 'ondemand',
         slidesToShow: 4,
         slidesToScroll: 2,
@@ -7,6 +7,37 @@ $(document).ready(function(){
         rows: 2,
         prevArrow: $(".slider-prew"),
         nextArrow: $(".slider-next"),
+        responsive: [
+            {
+                breakpoint: 960,
+                settings: {
+                    slidesToShow: 3,
+                    arrows: false
+                }
+            },
+            {
+                breakpoint: 540,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    arrows: false
+                }
+            },
+        ]
 
+    });
+
+    $("#burger").on("click", function() {
+        $(".main-menu").toggleClass("open");
+        $("body").toggleClass("noScroll");
+    });
+
+    $("#nav a").on("click", function() {
+        $(".main-menu").removeClass("open");
+        $("body").removeClass("noScroll");
+    });
+
+    $(".clickHere-button").on("click", function() {
+        alert("Thanks for your message. We will call you soon!");
     });
   });
